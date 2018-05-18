9e1a776d3a21620480aca09db9b205e04845681d
doubleface <christophe@cozycloud.cc>
2018-05-18 12:56:18 +0200

    Fetch bills as files

--- src/index.js
+++ src/index.js
@@ -1,127 +1,100 @@
 const {
   BaseKonnector,
   requestFactory,
-  signin,
+  log,
   scrape,
-  saveBills,
-  log
+  saveFiles,
+  errors
 } = require('cozy-konnector-libs')
 const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very usefull for
-  // debugging but very verbose. That is why it is commented out by default
   // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
   cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
   json: false,
-  // this allows request-promise to keep cookies between requests
   jar: true
 })
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://client.o2.fr'
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
-  log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
+  log('info', 'Fetching bills information')
+  const bills = await fetchAndParseBills()
+  log('info', 'Fetched bills information')
+
+  log('info', 'Downloading bills')
+  await saveFiles(bills, fields.folderPath)
 }
 
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if
-    validate: (statusCode, $) => {
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
-    }
-  })
-}
+async function fetchAndParseBills() {
+  let $ = await request(`${baseUrl}/factures/`)
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/cozy/cozy-konnector-libs/blob/master/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      url: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: url => `${baseUrl}/${url}`
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
-      }
-    },
-    'article'
+  const agencyId = $('#agence_id')
+    .text()
+    .trim()
+
+  const years = Array.from($('.year_invoice option')).map(option =>
+    $(option).val()
   )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // usefull for debugging or data migration
-      importDate: new Date(),
-      // document version, usefull for migration after change of document structure
-      version: 1
-    }
-  }))
+
+  let bills = []
+  for (const year of years) {
+    const $ = await request({
+      url: `${baseUrl}/wp-admin/admin-ajax.php`,
+      method: 'POST',
+      form: {
+        action: 'invoiceByYear',
+        agenceId: agencyId,
+        year
+      }
+    })
+    bills = bills.concat(
+      scrape(
+        $,
+        {
+          fileurl: {
+            sel: 'a',
+            attr: 'href',
+            parse: href => `${baseUrl}${href}`
+          },
+          filename: {
+            sel: 'a',
+            parse: text => `${text}.pdf`
+          }
+        },
+        'li'
+      )
+    )
+  }
+  return bills
 }
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.trim().replace('£', ''))
+async function authenticate(username, password) {
+  const request = requestFactory({ cheerio: false })
+  const result = await request({
+    url: `${baseUrl}/wp-admin/admin-ajax.php`,
+    method: 'POST',
+    form: {
+      action: 'ask_login',
+      login: username,
+      pwd: password
+    }
+  })
+
+  let json
+  try {
+    json = JSON.parse(result.trim())
+  } catch (err) {
+    log('error', 'Could not parse JSON response')
+    log('error', err.message)
+    throw new Error(errors.VENDOR_DOWN)
+  }
+
+  if (!json || json.state !== 'success') {
+    throw new Error(errors.LOGIN_FAILED)
+  }
 }
