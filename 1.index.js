7360f2aa14ec9184697847e17fe519efc0276a1f
Travis CI User <travis@example.org>
2019-03-20 16:14:34 +0000

    publish: chore: upgrade cozy libs
    generated from commit e69e49fa9da31a822dea7eb71e0bff6e7aff0364

--- 1.index.js
+++ 1.index.js
@@ -1,7 +1,7 @@
 exports.ids = [1];
 exports.modules = {
 
-/***/ 1473:
+/***/ 1262:
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
@@ -47059,7 +47059,7 @@ exports.MessageHandler = MessageHandler;
 
 /***/ }),
 
-/***/ 1474:
+/***/ 1263:
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
